8ca70ddc3e6a33a86f40bca318209c171d0307a3
Deraybcbc <121021917+Deraybcbc@users.noreply.github.com>
2024-10-21 17:26:21 +0200

    Creacion de las tablas de la base de datos

---
+++ Web/Laravel/takeAwayG6/database/migrations/2024_10_21_152304_create_comanda_articulos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComandaArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comanda_articulos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idProducto');
+            $table->string('talla');
+            $table->string('color');
+            $table->string('quantitat');
+            $table->float('preu');
+            $table->foreign('idProducto')->references('id')->on('productos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comanda_articulos');
+    }
+}
