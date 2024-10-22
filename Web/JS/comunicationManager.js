190d708435de85103632ed7caf0d232804557406
Deraybcbc <121021917+Deraybcbc@users.noreply.github.com>
2024-10-22 18:26:48 +0200

    Modificacion de archivos js

--- Web/JS/comunicationManager.js
+++ Web/JS/comunicationManager.js
@@ -2,16 +2,15 @@ export async function getProductoss() {
     try {
        const response = await fetch('http://localhost:8000/api/getProductos');
 
+       //CONSULTAR SI LA CONEXION ES BUENA
         if (!response.ok) {
             throw new Error(`Error: ${response.status} - ${response.statusText}`);
         }
 
         const data = await response.json();
         console.log("data");
-        
-        return data; // Devolver los datos de los productos
+        return data;
     } catch (error) {
         console.error('Error al obtener productos:', error);
-        throw error; // Opcional: puedes lanzar el error de nuevo si necesitas manejarlo en otro lugar
     }
 }
