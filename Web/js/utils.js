48f76d466ead10b7960b5bc63b4e64e7a2421c00
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-10-23 12:26:16 +0200

    merge completado

--- Web/js/utils.js
+++ Web/js/utils.js
@@ -1,25 +1,16 @@
 import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
-
+import { getProductoss } from './comunicationManager.js';
 createApp({
     setup() {
-        const categorias = ref([
-            { nombre: "zapatillas", imagen: "Img/zapatillas.jpeg" },
-            { nombre: "sudadera", imagen: "Img/sudadera.jpeg" },
-            { nombre: "pantalon", imagen: "Img/pantalon.jpeg" },
-            { nombre: "chaqueta", imagen: "Img/chaqueta.jpeg" },
-            { nombre: "camiseta", imagen: "Img/camiseta.jpeg" },
-            { nombre: "chandal", imagen: "Img/chandal.jpeg" },
-            { nombre: "chaleco", imagen: "Img/chaleco.jpeg" }
-        ]);
-        const categoriaFiltrada = ref('');
         const productos = ref([]);
+        const productos2 = ref([]);
         const productosEnCesta = ref([]);
-        const divActivo = ref('paginaDeInicio');
         const cestaActiva = ref(false);
         const finalitzaCompraActiva = ref(false);
-        const precioTotal = ref(0);
+        const precioTotal = ref(0); 
+        const paginaInicial = ref(true);
         function getProductos() {
-            fetch('./js/ropa.json')
+            fetch('./JS/ropa.json')
                 .then(response => response.json())
                 .then(data => {
                     productos.value = data;
@@ -27,74 +18,35 @@ createApp({
                 .catch(error => console.error('Error fetching productos:', error));
         }
 
+        function cambiarPantallaPrincipal(){
+            if (paginaInicial.value) {
+                paginaInicial.value = false;
+            }else{
+                paginaInicial.value = true;
+
+            }
+        }
+        async function cargarProductos(){
+            try {
+                const productos2 = await getProductoss();
+                console.log(productos2); // Aquí puedes hacer lo que necesites con los productos
+            } catch (error) {
+                console.error('No se pudieron cargar los productos:', error);
+            }
+        }
+
         function añadirALaCesta(index) {
             const productoSeleccionado = productos.value[index];
-            // productosEnCesta.value.push(productoSeleccionado);
-            const productoEnCesta = productosEnCesta.value.find(producto => producto.nombre ===productoSeleccionado.nombre);
-            if (productoEnCesta) {
-                productoEnCesta.cantidad++;
-            } else {
-                const numevoProducto = Object.assign({}, productoSeleccionado, { cantidad: 1});
-                productosEnCesta.value.push(numevoProducto);
-            }
-
+            productosEnCesta.value.push(productoSeleccionado);
             cestaActiva.value = true;
             finalitzaCompraActiva.value = true;
-            actualizarPrecioTotal();
             
             precioTotal.value += productoSeleccionado.precio;
         }
 
-        function restarCantidad(index) {
-            const producto = productosEnCesta.value[index];
-
-            if(producto.cantidad > 1){
-                producto.cantidad--;
-                actualizarPrecioTotal();
-            }
-        }
-
-        function sumaCantidad(index) {
-            const producto = productosEnCesta.value[index];
-            producto.cantidad++;
-            actualizarPrecioTotal();
-        }
-
-        function eliminarDesdeCarrito(index) {
-            productosEnCesta.value.splice(index,1);
-            actualizarPrecioTotal();
-
-            if(productoEnCesta.value.length === 0){
-                finalitzaCompraActiva.value = false;
-            }
-        }
-        
-        function actualizarPrecioTotal() {
-            precioTotal.value = 0;
-      
-            for (let i = 0; i < productosEnCesta.value.length; i++) {
-              precioTotal.value += productosEnCesta.value[i].precio * productosEnCesta.value[i].cantidad;
-            }
-          }
-        
-        function irABotiga(){
-            divActivo.value = 'paginaPrincipal';
-        }
-        function filtrarPorCategoria(categoria) {
-            categoriaFiltrada.value = categoria;
-            divActivo.value = 'paginaPrincipal';
-        }
-        function cambiarACarrito(){
-            divActivo.value = 'carrito';
-        }
-        function irPantallaInicio(){
-            divActivo.value = 'paginaDeInicio';
-        }
         function eliminarDeLaCesta(index) {
             const productoEliminado = productosEnCesta.value[index];
             productosEnCesta.value.splice(index, 1);
-
-            actualizarPrecioTotal();
 
             precioTotal.value -= productoEliminado.precio;
 
@@ -103,55 +55,30 @@ createApp({
             }
         }
 
-        function botonCesta() {
+        function toggleCesta() {
             if(cestaActiva.value){
             cestaActiva.value = false;
             }else{
                 cestaActiva.value = true;
             }
         }
-        function volverALaPaginaPrincipal(){
-            divActivo.value = 'paginaPrincipal';
-        }
-        function IrLogin() {
-            if (divActivo.value === 'divLogin') {
-                divActivo.value = 'paginaDeInicio';
-            } else {
-                divActivo.value = 'divLogin';
-            }
-        }
-        
-        function enviarComanda(){
-            
-        }
 
         onMounted(() => {
             getProductos();
+            cargarProductos();
         });
 
         return {
             productos,
             productosEnCesta,
             añadirALaCesta,
-            eliminarDesdeCarrito,
-            restarCantidad,
-            sumaCantidad,
-            actualizarPrecioTotal,
             eliminarDeLaCesta,
             cestaActiva,
-            botonCesta,
+            toggleCesta,
             finalitzaCompraActiva,
             precioTotal,
-            divActivo,
-            cambiarACarrito,
-            volverALaPaginaPrincipal,
-            irPantallaInicio,
-            irABotiga,
-            filtrarPorCategoria,
-            categorias,
-            categoriaFiltrada,
-            IrLogin,
-            enviarComanda
+            cambiarPantallaPrincipal,
+            paginaInicial,
         };
     }
 }).mount('#app');
