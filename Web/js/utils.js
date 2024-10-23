7a5e92497a9e5fd879be15f7a1d44051ae420a3a
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-10-23 21:38:57 +0200

    todo corregido

--- Web/js/utils.js
+++ Web/js/utils.js
@@ -12,13 +12,14 @@ createApp({
             { nombre: "chandal", imagen: "Img/chandal.jpeg" },
             { nombre: "chaleco", imagen: "Img/chaleco.jpeg" }
         ]);
-        const categoriaFiltrada = ref('');
+
         const productos = ref([]);
         const productosEnCesta = ref([]);
         const divActivo = ref('paginaDeInicio');
         const cestaActiva = ref(false);
         const finalitzaCompraActiva = ref(false);
         const precioTotal = ref(0);
+
         function getProductos() {
             fetch('./js/ropa.json')
                 .then(response => response.json())
@@ -27,38 +28,81 @@ createApp({
                 })
                 .catch(error => console.error('Error fetching productos:', error));
         }
-
-        function añadirALaCesta(index) {
-            const productoSeleccionado = productos.value[index];
-            productosEnCesta.value.push(productoSeleccionado);
-            cestaActiva.value = true;
-            finalitzaCompraActiva.value = true;
-            
-            precioTotal.value += productoSeleccionado.precio;
-        }
-        function irABotiga(){
-            divActivo.value = 'paginaPrincipal';
-        }
-        function filtrarPorCategoria(categoria) {
-            categoriaFiltrada.value = categoria;
-            divActivo.value = 'paginaPrincipal';
-        }
-        function cambiarACarrito(){
-            divActivo.value = 'carrito';
-            cestaActiva.value = false;
-        }
-        function irPantallaInicio(){
-            divActivo.value = 'paginaDeInicio';
-        }
         function eliminarDeLaCesta(index) {
             const productoEliminado = productosEnCesta.value[index];
             productosEnCesta.value.splice(index, 1);
+
+            actualizarPrecioTotal();
 
             precioTotal.value -= productoEliminado.precio;
 
             if (productosEnCesta.value.length === 0) {
                 finalitzaCompraActiva.value = false;
             }
+        }
+        function añadirALaCesta(index) {
+            const productoSeleccionado = productos.value[index];
+            const productoEnCesta = productosEnCesta.value.find(producto => producto.nombre === productoSeleccionado.nombre);
+
+            if (productoEnCesta) {
+                productoEnCesta.cantidad++;
+            } else {
+                const nuevoProducto = { ...productoSeleccionado, cantidad: 1 };
+                productosEnCesta.value.push(nuevoProducto);
+            }
+
+            cestaActiva.value = true;
+            finalitzaCompraActiva.value = true;
+            actualizarPrecioTotal();
+        }
+
+        function restarCantidad(index) {
+            const producto = productosEnCesta.value[index];
+            if (producto.cantidad > 1) {
+                producto.cantidad--;
+            } else {
+                eliminarDesdeCarrito(index); 
+            }
+            actualizarPrecioTotal();
+        }
+
+        function sumaCantidad(index) {
+            const producto = productosEnCesta.value[index];
+            producto.cantidad++;
+            actualizarPrecioTotal();
+        }
+
+        function eliminarDesdeCarrito(index) {
+            productosEnCesta.value.splice(index, 1);
+            actualizarPrecioTotal();
+
+            if (productosEnCesta.value.length === 0) {
+                finalitzaCompraActiva.value = false;
+            }
+        }
+
+        function actualizarPrecioTotal() {
+            precioTotal.value = productosEnCesta.value.reduce((total, producto) => {
+                return total + producto.precio * producto.cantidad;
+            }, 0);
+        }
+
+        function irABotiga() {
+            divActivo.value = 'paginaPrincipal';
+            cestaActiva.value = false;
+        }
+
+        function filtrarPorCategoria(categoria) {
+        }
+
+        function cambiarACarrito() {
+            divActivo.value = 'carrito';
+            cestaActiva.value = false;
+        }
+
+        function irPantallaInicio() {
+            divActivo.value = 'paginaDeInicio';
+            cestaActiva.value = false;
         }
 
         function botonCesta() {
@@ -68,17 +112,15 @@ createApp({
                 cestaActiva.value = true;
             }
         }
-        function volverALaPaginaPrincipal(){
+        function IrLogin(){
+            divActivo.value = 'divLogin';
+            cestaActiva.value = false;
+        }
+        function volverALaPaginaPrincipal() {
             divActivo.value = 'paginaPrincipal';
+            cestaActiva.value = false;
         }
-        function IrLogin() {
-            if (divActivo.value === 'divLogin') {
-                divActivo.value = 'paginaDeInicio';
-            } else {
-                divActivo.value = 'divLogin';
-            }
-        }
-        
+
         onMounted(() => {
             getProductos();
         });
@@ -87,7 +129,10 @@ createApp({
             productos,
             productosEnCesta,
             añadirALaCesta,
-            eliminarDeLaCesta,
+            eliminarDesdeCarrito,
+            restarCantidad,
+            sumaCantidad,
+            actualizarPrecioTotal,
             cestaActiva,
             botonCesta,
             finalitzaCompraActiva,
@@ -99,8 +144,8 @@ createApp({
             irABotiga,
             filtrarPorCategoria,
             categorias,
-            categoriaFiltrada,
-            IrLogin
+            IrLogin,
+            eliminarDeLaCesta
         };
     }
 }).mount('#app');
