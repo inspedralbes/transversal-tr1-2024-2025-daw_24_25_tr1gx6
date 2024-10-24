9c07a04e33a73006c74b8924e73c9f5ef6946511
Deraybcbc <a22kevburcac@inspedralbes.cat>
2024-10-24 11:55:40 +0200

    Modificacion de js comunicaction manager

--- Web/js/comunicationManager.js
+++ Web/js/comunicationManager.js
@@ -1,22 +1,25 @@
+const HOST = 'http://localhost:8000/api';
+
+
+
 export async function getProductoss() {
     try {
-        const response = await fetch('http://localhost:8000/api/getProductos');
+        const response = await fetch(HOST + '/getProductos');
 
         //CONSULTAR SI LA CONEXION ES BUENA
         if (!response.ok) {
             throw new Error(`Error: ${response.status} - ${response.statusText}`);
         }
         const data = await response.json();
-        console.log("Productos obtenidos:", data);
         return data;
     } catch (error) {
         console.error('Error al obtener productos:', error);
     }
 }
 
-export async function compraProductos(json) {
+export async function checkoutProductos(json) {
     try {
-        const response = await fetch('http://localhost/api/createComandaArt', {
+        const response = await fetch(HOST + '/createComandaArt', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -26,11 +29,76 @@ export async function compraProductos(json) {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const data = response.json();
+        const data = await response.json();
         console.log(data);
         return data;
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
-    
+
+}
+
+export async function checkout(json) {
+    try {
+        const response = await fetch(HOST + '/checkout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(json),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(console.error());
+    }
+}
+
+export async function pedidoUser(json) {
+    try {
+        const response = await fetch(HOST + '/pedidoUser', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(json),
+        });
+        //CONSULTAR SI LA CONEXION ES BUENA
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+export async function getCategorias() {
+    try{
+        const response = await fetch(HOST + '/getCategory');
+        const data = await response.json();
+        return data;
+
+    }catch(error){
+        console.log(error);
+    }   
+}
+
+export async function getMarcas() {
+    try{
+        const response = await fetch(HOST + '/getMarcas');
+        const data = await response.json();
+        return data;
+
+    }catch(error){
+        console.log(error);
+    }   
 }
