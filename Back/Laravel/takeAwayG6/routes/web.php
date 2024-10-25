0aa46f028eeb371f9e30400f9c59079c1c3aeabf
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-10-25 11:57:44 +0200

    rutas para categorias y users creadas

--- Back/Laravel/takeAwayG6/routes/web.php
+++ Back/Laravel/takeAwayG6/routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
 
 Route::get('/getProductos',[ProductoController::class, 'getProductos'])->name('get-productos');
 //Route::post('/getProductos',[ProductoController::class, 'updateProducto'])->name('get-productos');
+Route::post('/deleteCategory',[CategoriaController::class, 'DeleteCategory'])->name('delete.category');
+Route::post('/updateCategory',[CategoriaController::class, 'UpdateCategory'])->name('update.category');
+Route::post('/createCategory',[CategoriaController::class, 'CreateCategory'])->name('create.category');
+Route::post('/getCategory',[CategoriaController::class, 'getCategory'])->name('get.category');
+Route::post('/getUsers',[CategoriaController::class, 'getUsers'])->name('get.users');
+Route::post('/createUser',[CategoriaController::class, 'createUser'])->name('create.user');
+Route::post('/updateUser',[CategoriaController::class, 'updateUser'])->name('update.user');
+Route::post('/deleteUser',[CategoriaController::class, 'deleteUser'])->name('deleteUser');
+
