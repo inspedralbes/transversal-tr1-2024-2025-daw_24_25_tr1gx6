53942b6d452dbd8c02dcf473a893cece49685a77
a19pabmatpav <a19pabmatpav@inspedralbes.cat>
2024-11-05 10:49:34 +0100

    mas avances

--- Back/Laravel/takeAwayG6/app/Http/Controllers/MailController.php
+++ Back/Laravel/takeAwayG6/app/Http/Controllers/MailController.php
@@ -17,33 +17,40 @@ class MailController extends Controller
     public function sendMail(Request $request) {
         $type = $request->input('type');
         $email = 'a19pabmatpav@inspedralbes.cat'; 
+        try {
+            switch ($type) {
+                case 'confirm':
+                    Mail::to($email)->send(new estadoConfirm);
+                    break;
+                case 'confirmed':
+                    Mail::to($email)->send(new estadoConfirmed);
+                    break;
+                case 'preparando':
+                    Mail::to($email)->send(new estadoPreparando);
+                    break;
+                case 'preparado':
+                    Mail::to($email)->send(new estadoPreparado);
+                    break;
+                case 'enviado':
+                    Mail::to($email)->send(new estadoEnviado);
+                    break;
+                case 'reparto':
+                    Mail::to($email)->send(new estadoReparto);
+                    break;
+                case 'entregado':
+                    Mail::to($email)->send(new estadoEntregado);
+                    break;
+                default:
+                    return response()->json(['message' => 'Tipo de correo no reconocido'], 400);
+            }
 
-        switch ($type) {
-            case 'confirm':
-                Mail::to($email)->send(new estadoConfirm);
-                break;
-            case 'confirmed':
-                Mail::to($email)->send(new estadoConfirmed);
-                break;
-            case 'preparando':
-                Mail::to($email)->send(new estadoPreparando);
-                break;
-            case 'preparado':
-                Mail::to($email)->send(new estadoPreparado);
-                break;
-            case 'enviado':
-                Mail::to($email)->send(new estadoEnviado);
-                break;
-            case 'reparto':
-                Mail::to($email)->send(new estadoReparto);
-                break;
-            case 'entregado':
-                Mail::to($email)->send(new estadoEntregado);
-                break;
-            default:
-                return response()->json(['message' => 'Tipus de correu no reconegut'], 400);
+            // Respuesta exitosa
+            return response()->json(['message' => 'Mensaje enviado correctamente']);
+
+        } catch (\Exception $e) {
+            // Loguea el error y devuelve una respuesta de error
+            \Log::error('Error al enviar el correo: ' . $e->getMessage());
+            return response()->json(['error' => 'Error al enviar el correo'], 500);
         }
-
-        return response()->json(['message' => 'Missatge enviat correctament']);
     }
 }
